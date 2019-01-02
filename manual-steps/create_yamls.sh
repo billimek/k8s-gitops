39af66ca1726c9cb49caf1df0b96b41047a3c139
Jeff Billimek <jeff@billimek.com>
2019-01-01 20:16:23 -0500

    handle manual yaml actions

---
+++ manual-steps/create_yamls.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ -z "$DOMAIN" ]]; then
+  echo ".env does not appear to be sourced, sourcing now"
+  . ../.env
+fi
+
+kapply() {
+  envsubst < "$@" | kubectl apply -f -
+}
+
+
+###################
+# traefik-external
+###################
+for i in yamls/traefik-external/*.txt
+do
+  kapply $i
+done
