3940dcb17c29097e5d0f498a0aa8b1c58d1117e4
Jeff Billimek <jeff@billimek.com>
2019-02-10 01:14:32 -0500

    deploy rabbitmq

--- manual-steps/create_secrets.sh
+++ manual-steps/create_secrets.sh
@@ -58,4 +58,5 @@ kseal values-to-encrypt/hass-mysql-values.txt > ../secrets/hass-mysql-values.yam
 kseal values-to-encrypt/unifi-values.txt > ../secrets/unifi-values.yaml
 kseal values-to-encrypt/unifi-externaldb-values.txt > ../secrets/unifi-externaldb-values.yaml
 kseal values-to-encrypt/node-red-values.txt > ../secrets/node-red-values.yaml
+kseal values-to-encrypt/rabbitmq-values.txt > ../secrets/rabbitmq-values.yaml
 
