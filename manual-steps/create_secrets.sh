26b882ee48bc4e1b8a063a6f58e1e42e8e64ee29
Jeff Billimek <jeff@billimek.com>
2019-02-12 21:40:05 -0500

    remove un-needed unifi deployment

--- manual-steps/create_secrets.sh
+++ manual-steps/create_secrets.sh
@@ -56,7 +56,6 @@ kseal values-to-encrypt/sonarr-values.txt > ../secrets/sonarr-values.yaml
 kseal values-to-encrypt/hass-values.txt > ../secrets/hass-values.yaml
 kseal values-to-encrypt/hass-mysql-values.txt > ../secrets/hass-mysql-values.yaml
 kseal values-to-encrypt/unifi-values.txt > ../secrets/unifi-values.yaml
-kseal values-to-encrypt/unifi-externaldb-values.txt > ../secrets/unifi-externaldb-values.yaml
 kseal values-to-encrypt/node-red-values.txt > ../secrets/node-red-values.yaml
 kseal values-to-encrypt/rabbitmq-values.txt > ../secrets/rabbitmq-values.yaml
 kseal values-to-encrypt/nextcloud-values.txt > ../secrets/nextcloud-values.yaml
