90ada94046aaa151ccf13b49616d96d4e94f33d0
Jeff Billimek <jeff@billimek.com>
2019-10-12 22:24:51 -0400

    add oauth2-proxy

--- setup/bootstrap-vault.sh
+++ setup/bootstrap-vault.sh
@@ -154,6 +154,7 @@ loadSecretsToVault() {
   # helm chart values
   ####################
   kvault "kube-system/kured/kured-helm-values.txt"
+  kvault "kube-system/oauth2-proxy/oauth2-proxy-helm-values.txt"
   kvault "logs/kibana/kibana-helm-values.txt"
   kvault "monitoring/chronograf/chronograf-helm-values.txt"
   kvault "monitoring/comcast/comcast-helm-values.txt"
