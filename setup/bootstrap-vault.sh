74786c2777701777ba5f14d5448cc50ce8e8ef65
Jeff Billimek <jeff@billimek.com>
2021-08-12 14:59:10 -0400

    temporarily exclude router from prom scraping
    Signed-off-by: Jeff Billimek <jeff@billimek.com>

--- setup/bootstrap-vault.sh
+++ setup/bootstrap-vault.sh
@@ -196,8 +196,7 @@ loadSecretsToVault() {
 
 loadSecretsToVault-oneoff() {
   message "writing secrets to vault"
-  # kvault "monitoring/kube-prometheus-stack/kube-prometheus-stack-helm-values.txt"
-  kvault "default/frigate/frigate-helm-values.txt"
+  kvault "monitoring/kube-prometheus-stack/kube-prometheus-stack-helm-values.txt"
 
 }
 
