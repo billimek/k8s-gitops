af8ef60ef8a9ca20d4c3e5b9847545f866fc5bbe
Jeff Billimek <jeff@billimek.com>
2021-08-22 09:07:50 -0400

    yeet old minio
    Signed-off-by: Jeff Billimek <jeff@billimek.com>

--- setup/bootstrap-vault.sh
+++ setup/bootstrap-vault.sh
@@ -185,7 +185,6 @@ loadSecretsToVault() {
   kvault "default/emqx/emqx-helm-values.txt"
   kvault "default/frigate/frigate-helm-values.txt"
   kvault "default/home-assistant/home-assistant-helm-values.txt"
-  kvault "default/minio/minio-helm-values.txt"
   kvault "default/monica/monica-helm-values.txt"
   kvault "default/plex/plex-helm-values.txt"
   kvault "default/rtorrent-flood/rtorrent-flood-helm-values.txt"
