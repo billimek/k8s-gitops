950b9913413c666ac21ebe3e81f3de2222b0fec4
Jeff Billimek <jeff@billimek.com>
2021-08-22 10:58:45 -0400

    migrate to new loki & promtail charts
    Signed-off-by: Jeff Billimek <jeff@billimek.com>

--- setup/bootstrap-vault.sh
+++ setup/bootstrap-vault.sh
@@ -197,8 +197,6 @@ loadSecretsToVault-oneoff() {
   message "writing secrets to vault"
   # kvault "monitoring/kube-prometheus-stack/kube-prometheus-stack-helm-values.txt"
   kvault "logs/loki/loki-helm-values.txt"
-  kvault "monitoring/thanos/thanos-helm-values.txt"
-  kvault "velero/velero/velero-helm-values.txt"
   
 
 
