278426701e8f1739c3ca8fc12e4d5277aece8a14
Jeff Billimek <jeff@billimek.com>
2023-01-28 16:34:29 -0500

    migrate uptimerobot to external-secrets
    Signed-off-by: Jeff Billimek <jeff@billimek.com>

--- setup/bootstrap-vault.sh
+++ setup/bootstrap-vault.sh
@@ -181,7 +181,7 @@ loadSecretsToVault() {
   # kvault "monitoring/grafana/grafana-helm-values.txt"
   # kvault "monitoring/kube-prometheus-stack/kube-prometheus-stack-helm-values.txt"
   # kvault "monitoring/thanos/thanos-helm-values.txt"
-  kvault "monitoring/uptimerobot-prometheus/uptimerobot-prometheus-helm-values.txt"
+  # kvault "monitoring/uptimerobot-prometheus/uptimerobot-prometheus-helm-values.txt"
   # kvault "default/emqx/emqx-helm-values.txt"
   # kvault "default/frigate/frigate-helm-values.txt"
   # kvault "default/home-assistant/home-assistant-helm-values.txt"
