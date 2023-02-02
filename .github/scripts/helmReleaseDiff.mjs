1cdd6910fdf23e5c9602da12bcab6430d690292e
Jeff Billimek <jeff@billimek.com>
2023-02-01 20:37:13 -0500

    pick 1:
    * color diffs (`diff`)
    * nicely formatted diffs (`dyff`)
    
    ... can't have both, sadly.
    
    Signed-off-by: Jeff Billimek <jeff@billimek.com>

--- .github/scripts/helmReleaseDiff.mjs
+++ .github/scripts/helmReleaseDiff.mjs
@@ -107,5 +107,5 @@ const incomingManifests = await helmTemplate(
 )
 
 // Print diff using dyff
-const diff = await $`${dyff} --color=on --truecolor=off between --omit-header --ignore-order-changes --detect-kubernetes=true --output=human ${currentManifests} ${incomingManifests}`
+const diff = await $`${dyff} --color=off --truecolor=off between --omit-header --ignore-order-changes --detect-kubernetes=true --output=human ${currentManifests} ${incomingManifests}`
 echo(diff.stdout.trim())
